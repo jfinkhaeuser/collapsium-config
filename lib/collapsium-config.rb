099ba53beb836bdd4af71652a09995a6c25ce360
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-05-11 23:01:15 +0200

    Project skeleton

---
+++ lib/collapsium-config.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+#
+# collapsium-config
+# https://github.com/jfinkhaeuser/collapsium-config
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other collapsium-config contributors.
+# All rights reserved.
+#
+
+require 'collapsium-config/version'
---
+++ lib/collapsium-config/version.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+#
+# collapsium-config
+# https://github.com/jfinkhaeuser/collapsium-config
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other collapsium-config contributors.
+# All rights reserved.
+#
+module Collapsium
+  module Config
+    # The current release version
+    VERSION = "0.1.0".freeze
+  end # module Config
+end # module Collapsium
